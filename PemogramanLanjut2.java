14217a6acd7e903270f6dbfad00f75e718a9b931
Raviko Muhammad Raihan Yusi <101442920+RavikoMuhammadRaihanYusi12@users.noreply.github.com>
2022-06-15 21:38:16 +0700

    Add files via upload

---
+++ PemogramanLanjut2.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+
+/**
+ *
+ * @author kotzy pc
+ */
+public class PemogramanLanjut2 {
+    String nopolisi;
+    String model;
+    int TahunProduksi;
+    String Warna;
+    
+    public PemogramanLanjut2(String mdl, String nopol, int tahun){
+       nopolisi = nopol;
+       mdl = model;
+       TahunProduksi = tahun;  
+    }
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
